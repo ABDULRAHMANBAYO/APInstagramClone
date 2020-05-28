0d7fea29cb9e7e2a5d3c9cae47e352c613189e7f
ABDUL RAHMAN <ramhziworld@gmail.com>
2020-05-29 00:58:21 +0100

    Added fancy toast sdk

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -3,8 +3,15 @@ package com.example.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUp extends AppCompatActivity {
 
@@ -14,5 +21,23 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         // Save the current Installation to Back4App
         ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+    public void helloWordTapped(View v)
+    {
+        ParseObject boxer = new ParseObject("Boxer");
+        boxer.put("punch_speed",200);
+        boxer.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null)
+                {
+                    FancyToast.makeText(SignUp.this,"Boxer object saved",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+//                    Toast.makeText(SignUp.this, "Boxer object saved", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    FancyToast.makeText(SignUp.this,"Something went wrong",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                }
+            }
+        });
     }
 }
